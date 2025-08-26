ca764018d658d35fcb5722de0ef391933e099ae8
Hafiz Muhammad Ahmad Tahir <countergamerz@gmail.com>
2025-08-27 02:06:51 +0500

    Update Api.js

--- webapp/src/Api.js
+++ webapp/src/Api.js
@@ -1,6 +1,9 @@
+// The base URL will be set by an environment variable on the hosting platform
+const API_URL = process.env.REACT_APP_API_URL;
+
 /// Api request to fetch documents with multiple filters
 export async function fetchItems(filters) {
-  const response = await fetch(`${process.env.REACT_APP_API_URL}/api/fetchItems`, {
+  const response = await fetch(`${API_URL}/api/fetchItems`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(filters),
@@ -10,12 +13,12 @@ export async function fetchItems(filters) {
 
 /// Api request to fetch all available Tags (Diseases/Specialties)
 export async function fetchTags() {
-  const response = await fetch(`${process.env.REACT_APP_API_URL}/api/fetchTags`);
+  const response = await fetch(`${API_URL}/api/fetchTags`);
   return await response.json();
 }
 
 /// Api request to fetch all available Publication Types
 export async function fetchPublicationTypes() {
-  const response = await fetch(`${process.env.REACT_APP_API_URL}/api/fetchPublicationTypes`);
+  const response = await fetch(`${API_URL}/api/fetchPublicationTypes`);
   return await response.json();
 }
